da7cbff2e0fe9e4de1910ec1dba8d2f638c46ce4
Alfredo Salazar <>
2019-11-19 08:07:44 -0600

    Added CORS

--- server.js
+++ server.js
@@ -9,6 +9,16 @@ let app = express();
 let jsonParser = bodyParser.json();
 mongoose.Promise = global.Promise;
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
+  if (req.method === "OPTIONS") {
+    return res.send(204);
+  }
+  next();
+});
+
 app.use( express.static( "public" ) );
 
 app.use( morgan( "dev" ) );
